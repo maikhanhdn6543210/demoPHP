84521c6a07160b99afa6eb4055377c4d67707070
Mai Khanh <maikhanh@Mais-MacBook-Pro.local>
2018-09-11 19:25:23 +0700

    BT3

--- session1/array.php
+++ session1/array.php
@@ -1,4 +1,28 @@
-<?php 
+<?php
     $arr = array();
+    
     var_dump($arr);     //Use for debug
+    echo "<br/>";
+    $arr = array('Trung', 'Huyen', 'Khanh');
+    $arr1 = array(0 =>'Trung', 1=>'Huyen', 2=>'Khanh');
+    $arr2 = array(0 =>'Trung', 1=>'Huyen', 4=>'Khanh');
+
+    var_dump($arr);
+    echo "<br/>";
+    var_dump($arr1);
+    echo "<br/>";
+    var_dump($arr2);
+    echo "<br/>";
+
+    foreach ($arr1 as $key => $value) {
+        echo $key;
+        echo "<br/>";
+        echo $value;
+        echo "<br/>";
+    }
+
+    //Them 1 phan tu vao mang array_push
+    array_push($arr1,'Hung');
+
+
 ?>
---
+++ session1/session2_BT3.php
@@ -0,0 +1,24 @@
+<?php
+    
+
+    function print_product($product)
+    {
+        foreach ($product as $key => $value) {
+            echo $value;
+            echo "<br/>";
+        }
+    }
+    echo "<br>-------1: Print Product-------<br>";
+    $product = array('BanhKeo', 'TraiCay', 'DoChoi');
+    print_product($product);
+    echo "<br>-------2: Add AoQuan-------<br>";
+     //them mot phan tu vao mang bang array_push
+    array_push($product, 'Ao Quan');
+    print_product($product);
+    echo "<br>-------3: Update BanhKeo to ThucPham-------<br>";
+    $product['0'] = 'ThucPham';
+    print_product($product);
+    echo "<br>-------4: Delete TraiCay-------<br>";
+    unset($product['1']);
+    print_product($product);
+
